3c989907a894eceb7e78799cb0b9ff7523bd8aa5
JRempel <Jonathan.Rempel@dal.ca>
2016-01-06 23:06:54 -0400

    Refactor & bug fix
    Moved converting ASCII value to proper base value into its own methods,
    updated several variable names, fixed a bug where the index for adding
    to the bitset wasn't updated when the base10 value was equal to 0.

--- src/set1/BaseConvert.java
+++ src/set1/BaseConvert.java
@@ -5,13 +5,34 @@ import java.util.BitSet;
 // NOTE: Does not pad the result
 
 public class BaseConvert {
-	private static final Character[] base64Order = new Character[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+	public static final Character[] base64Order = new Character[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
 			'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e',
 			'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 			'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/', };
 
-	private static final Character[] hexOrder = new Character[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a',
+	public static final Character[] hexOrder = new Character[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a',
 			'b', 'c', 'd', 'e', 'f' };
+
+	public static int getHexValue(char value) {
+		if (value > 57)
+			value -= 39;
+		value -= 48;
+		return value;
+	}
+
+	public static int getBase64Value(char value) {
+		if (value >= 'A' && value <= 'Z')
+			value -= 65;
+		else if (value >= 'a' && value <= 'z')
+			value -= 71;
+		else if (value >= '0' && value <= '9')
+			value += 4;
+		else if (value == '+')
+			value += 19;
+		else if (value == '/')
+			value += 16;
+		return value;
+	}
 
 	public static String hexToBase64(String hex) throws Exception {
 		hex = hex.toLowerCase();
@@ -28,22 +49,17 @@ public class BaseConvert {
 			// decimal value
 			for (int i = 0; i < hex.length(); i += 2) {
 				// get actual value from ASCII table
-				char1 = hex.charAt(i);
-				char2 = hex.charAt(i + 1);
-				if (char1 > 57)
-					char1 -= 39;
-				if (char2 > 57)
-					char2 -= 39;
-				char1 -= 48;
-				char2 -= 48;
+				char1 = getHexValue(hex.charAt(i));
+				char2 = getHexValue(hex.charAt(i + 1));
+
 				// merge characters to a single integer value
 				char1 <<= 4;
 				base10 = (char1 | char2);
-				
+
 				// loop that adds to the BitSet reads right-to-left, so reverse
 				// the bits
 				base10 = Integer.reverse(base10 << 24) & 0xff;
-				
+
 				// append binary values to the BitSet
 				// and account for when base10 is 0
 				if (base10 == 0L) {
@@ -79,34 +95,29 @@ public class BaseConvert {
 		if (base64.length() % 4 != 0 || base64.contains("[^a-zA-Z0-9\\+/]"))
 			throw new Exception("InputNotBase64");
 		else {
-			int charValue = 0;
+			int base10 = 0;
 			int index = 0;
 			String hex = "";
 			BitSet bits = new BitSet();
 			for (int i = 0; i < base64.length(); i++) {
-				charValue = base64.charAt(i);
 				// get actual value from ASCII table
-				if (charValue >= 'A' && charValue <= 'Z')
-					charValue -= 'A';
-				else if (charValue >= 'a' && charValue <= 'z')
-					charValue -= 71;
-				else if (charValue >= '0' && charValue <= '9')
-					charValue += 4;
-				else if (charValue == '+')
-					charValue += 19;
-				else if (charValue == '/')
-					charValue += 16;
+				base10 = getBase64Value(base64.charAt(i));
+
 				/// loop that adds to the BitSet reads right-to-left, so reverse
 				// the bits and then shift
-				charValue = Integer.reverse(charValue << 24) & 0xff;
-				charValue >>= 2;
+				base10 = Integer.reverse(base10 << 24) & 0xff;
+				base10 >>= 2;
 				// append binary values to the BitSet
-				while (charValue != 0L) {
-					if (charValue % 2 != 0) {
-						bits.set(index);
+				if (base10 == 0) {
+					index += 6;
+				} else {
+					while (base10 != 0L) {
+						if (base10 % 2 != 0) {
+							bits.set(index);
+						}
+						index++;
+						base10 >>= 1;
 					}
-					index++;
-					charValue >>= 1;
 				}
 				// account for trailing 0s
 				while (index % 6 != 0) {
@@ -117,17 +128,17 @@ public class BaseConvert {
 			String temp;
 			int remainder;
 			for (int i = 0; i < index; i++) {
-				charValue = (charValue | (bits.get(i) ? 1 : 0));
+				base10 |= (bits.get(i) ? 1 : 0);
 				if ((i + 1) % 8 == 0) {
 					temp = "";
-					while (charValue != 0L) {
-						remainder = charValue % 16;	
+					while (base10 != 0L) {
+						remainder = base10 % 16;
 						temp = hexOrder[remainder] + temp;
-						charValue /= 16;
+						base10 /= 16;
 					}
 					hex += temp;
 				}
-				charValue <<= 1;
+				base10 <<= 1;
 			}
 			return hex;
 		}
